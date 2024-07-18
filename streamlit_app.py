140bf725fc017e7747cb4120098340b1b3244a9e
yoongseong <hys_3@yahoo.com>
2024-07-18 16:05:59 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ import requests
 TE_BASE_URL = "https://api.thousandeyes.com/v7/"
 TE_HEADERS = {
     "Authorization": f"Bearer {st.secrets["TE_TOKEN"]}",
+    # "Authorization": f"Bearer ",
     "Content-Type": "application/json",
     "Accept": "application/hal+json"
 }
@@ -22,26 +23,73 @@ TE_PAYLOAD = '''{
     }
 }'''
 TE_TEST_ID = "289949"
-TE_WINDOW = "30m"
+TE_AGENT_ID = "9db4eabc-a86d-4fc9-b666-6ea6e8764144"
+TE_WINDOW = "1h"
 
 st.logo("gallery/logo.png")
 st.title(":desktop_computer: NTT Com DD Dashboard")
 
 @st.cache_data
-def show_availability():
+def get_te_network_data():
     url = f"{TE_BASE_URL}endpoint/test-results/scheduled-tests/{TE_TEST_ID}/network/filter?window={TE_WINDOW}"
     response = requests.request('POST', url, headers=TE_HEADERS, data = TE_PAYLOAD)
     # st.json(response.text.encode('utf8'))
     # st.write(response.json())
-    data = response.json()["results"]
-    filtered_data = []
-    target_keys = ["avgLatency", "jitter"]
-    for result in data:
-        filtered_data.append({key: result[key] for key in target_keys} )    
-    df = pd.DataFrame(filtered_data)
-    df
+    return response.json()["results"]
+    
 
-show_availability()
+@st.cache_data
+def get_te_http_data():
+    url = f"{TE_BASE_URL}endpoint/test-results/scheduled-tests/{TE_TEST_ID}/http-server?window={TE_WINDOW}"
+    response = requests.request('GET', url, headers=TE_HEADERS, data = None)
+    # st.json(response.text.encode('utf8'))
+    # st.write(response.json())
+    data = response.json()["results"]
+    return [result for result in data if result["agentId"] == TE_AGENT_ID]
+    
+
+st.header("Webex Performance Chart")
+
+data = get_te_http_data()
+
+st.subheader("Response Time")
+filtered_data = []
+target_keys = ["responseTime"]
+for result in data:
+    filtered_data.append({key: result[key] for key in target_keys} )
+df = pd.DataFrame(filtered_data)
+st.line_chart(df, y_label="milliseconds")
+
+data = get_te_network_data()
+
+st.subheader("Packet Loss")
+filtered_data = []
+target_keys = ["loss"]
+for result in data:
+    filtered_data.append({key: result[key] for key in target_keys} )
+df = pd.DataFrame(filtered_data)
+st.line_chart(df, y_label="%")
+
+st.subheader("Average Latency")
+filtered_data = []
+target_keys = ["avgLatency"]
+for result in data:
+    filtered_data.append({key: result[key] for key in target_keys} )
+df = pd.DataFrame(filtered_data)
+st.line_chart(df, y_label="milliseconds")
+
+st.subheader("Jitter")
+filtered_data = []
+target_keys = ["jitter"]
+for result in data:
+    filtered_data.append({key: result[key] for key in target_keys} )
+df = pd.DataFrame(filtered_data)
+st.line_chart(df, y_label="milliseconds")
+
+
+
+
+
 
 # with st.container():
 #     st.header("Availability")
